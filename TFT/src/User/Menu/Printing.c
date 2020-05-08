a49900bfd586026098cd080fb3e6fd54a49a7a0c
rebel1 <453277+rebel1@users.noreply.github.com>
2020-05-08 16:01:55 +0200

    Update Printing.c

--- TFT/src/User/Menu/Printing.c
+++ TFT/src/User/Menu/Printing.c
@@ -705,9 +705,7 @@ void abortPrinting(void)
 
     case TFT_UDISK:
     case TFT_SD:
-      if (infoSettings.send_cancel_gcode == 1)
-        mustStoreCmd(PRINT_CANCEL_GCODE);
-
+      
       clearCmdQueue();
       break;
   }
